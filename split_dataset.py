d0e72086921cf158094d4558ca64fe627ebeb9d4
AlexSutila <alexsutila2020@gmail.com>
2023-02-16 17:39:25 -0700

    Seeded randomization

--- split_dataset.py
+++ split_dataset.py
@@ -1,10 +1,17 @@
 #!/bin/python3
 
 import pandas as pd
+import numpy as np
 
+np.random.seed(8)
 df = pd.read_csv('data/USCensus1990.csv')
-df1, df2 = df.sample(frac=0.5), df.drop(df.sample(frac=0.5).index)
 
-df1.to_csv('data/USCensus1990_1.csv')
-df2.to_csv('data/USCensus1990_2.csv')
+shuffle = np.random.permutation(df.index)
+half = int(len(shuffle) / 2)
+
+idx_1, idx_2 = shuffle[:half], shuffle[half:]
+df1, df2 = df.loc[idx_1], df.loc[idx_2]
+
+df1.to_csv('data/USCensus1990_1.csv', index=False)
+df2.to_csv('data/USCensus1990_2.csv', index=False)
 
